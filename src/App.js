6ae9c7beb58ea3348cfabdef1eec2a4c2b684b69
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-05-14 16:08:15 +0530

    middle

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
       {
         id: 3,
         title: "Make some delicious fried rice",
-        completed: true,
+        completed: false,
       },
       {
         id: 4,
@@ -29,11 +29,34 @@ class App extends Component {
       },
     ],
   };
+
+  //Complete toggle
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  //Delete todo
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
   render() {
-    console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,16 +11,20 @@ export class TodoItem extends Component {
     };
   };
 
-  markComplete = (e) => {
-    console.log(this.props);
-  };
-
   render() {
+    const { id, title } = this.props.todo;
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.markComplete} /> {""}
-          {this.props.todo.title}
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {""}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            X
+          </button>
         </p>
       </div>
     );
@@ -30,5 +34,14 @@ export class TodoItem extends Component {
 //Proptypes
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
+};
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  cursor: "pointer",
+  float: "right",
 };
 export default TodoItem;
